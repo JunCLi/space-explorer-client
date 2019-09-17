9f1abc7bfed48ae7bddba4421a423aa8190f3e91
JunCLi <kevinjcli1994@gmail.com>
2019-09-17 17:07:03 -0400

    reorganized and separated react-native init code in App.js

---
+++ js/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react'
+
+import Temp from './components/temp'
+
+const App = () => {
+  return (
+    <Temp />
+  );
+};
+
+export default App;
